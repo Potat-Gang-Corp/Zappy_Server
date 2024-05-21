33be7acf4d6d2a26db8b44e0d6e52259ab2488ab
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-05-21 14:15:51 +0200

    [+] add Doxyfile in the .gitignore

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['add_2ec_0',['add.c',['../add_8c.html',1,'']]],
+  ['add_5ffunc_1',['add_func',['../add_8c.html#aa066895022b8d4b5e2a48a868edf0896',1,'add.c']]]
+];
---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['add_5ffunc_6',['add_func',['../add_8c.html#aa066895022b8d4b5e2a48a868edf0896',1,'add.c']]]
+];

732dbdb6a79ec6858b78b12149ba4454c644f1b2
Emna <emna.boukadida@epitech.eu>
2024-05-24 12:05:05 +0200

    [+] a test for the map

--- tests/unit-tests/test_map.c
+++ tests/unit-tests/test_map.c
@@ -9,3 +9,13 @@
 #include <criterion/redirect.h>
 #include <criterion/assert.h>
 #include "../../include/my.h"
+
+Test(init_map, test_init_map)
+{
+    map_t *map = initialize_map(10, 10);
+
+    cr_assert_not_null(map);
+    cr_assert_eq(map->width, 10);
+    cr_assert_eq(map->height, 10);
+    cr_assert_not_null(map->tiles);
+}

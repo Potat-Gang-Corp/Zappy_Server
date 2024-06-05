b136d1061bd6c5bf0ce7da461460f09b7d869234
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-06-05 15:57:35 +0200

    [-] rm time include

--- include/my.h
+++ include/my.h
@@ -30,7 +30,6 @@
     #include <arpa/inet.h>
     #include <sys/time.h>
     #include <time.h>
-    #include <linux/time.h>
 
 int add_func(int a, int b);
 void second_init_map(map_t *map, int x, int y, int width);

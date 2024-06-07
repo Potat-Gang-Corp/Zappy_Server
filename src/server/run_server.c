3705a90fbc033a014e700b0f54a24ebd07facd09
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-06-07 17:38:51 +0200

    [+] psu for VM

--- src/server/run_server.c
+++ src/server/run_server.c
@@ -24,19 +24,22 @@ void handle_sigint(int sig)
 int run_server(void)
 {
     server_t *server = get_instance();
-    //game_t *game = get_game_instance();
-    //game->freq;
+    clock_t time_req;
+    game_t *game = get_instance();
 
     if (server == NULL)
         return 84;
     printf("server launched on port %d\n", server->port);
-    signal(SIGINT, handle_sigint); // la tu initialises ta clock
-    while (1) { // tu démarres la clock
+    signal(SIGINT, handle_sigint);
+    //time_req = clock();
+    while (1) {
         select_loop();
         if (FD_ISSET(server->socket, &server->readfs))
             accept_new_client();
         handle_clients(); //la jrecup les commandes
-        execute_cli_cmd(); // la dedans quand tu vas boucler sur tes commandes, celles deja en cours vont avoir leur cd réduit de 1
+        execute_cli_cmd();
+        if (clock() - time_req != 1 / game->freq)
+            wait(); // la dedans quand tu vas boucler sur tes commandes, celles deja en cours vont avoir leur cd réduit de 1
     } // ici tu regardes si ta clock moins la clock atm est >= a 1/f sinon boucle jusqu'a l'atteindre
     //okay donc au finale ma vraie question c'était ça, je fais des tours de durée 1/f // c'était ma pièce de puzzle manquante
     //merci je me sent mieux

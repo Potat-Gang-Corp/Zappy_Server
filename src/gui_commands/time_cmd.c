92808ddb4bc254e7dc6f299a4abb6b0308f6a99d
Noah Gibelli <noah.gibelli@epitech.eu>
2024-06-17 15:29:37 +0200

    [+] Implementation of the sst and sgt commands for the graphic client

--- src/gui_commands/time_cmd.c
+++ src/gui_commands/time_cmd.c
@@ -5,16 +5,28 @@
 ** time_cmd
 */
 
+
+#include "my.h"
+#include "get_instance.h"
+
 int cmd_sgt(char *command_type, int gui_socket)
 {
+    game_t *game = get_game_instance();
+
     (void)command_type;
-    (void)gui_socket;
+    dprintf(gui_socket, "sgt %d\n", game->freq);
     return 0;
 }
 
 int cmd_sst(char *command_type, int gui_socket)
 {
-    (void)command_type;
-    (void)gui_socket;
+    game_t *game = get_game_instance();
+    char *time_parameter = strtok(command_type, " ");
+    int time_nb;
+
+    time_parameter = strtok(NULL, " #");
+    time_nb = atoi(time_parameter);
+    game->freq = time_nb;
+    dprintf(gui_socket, "sst %d\n", time_nb);
     return 0;
 }

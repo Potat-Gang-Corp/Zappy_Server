c5036948f4d65d858a4976d4e79b9a4d3723022f
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-06-21 18:40:21 +0200

    [-] fixed coding-style

--- src/cli_commands/move_cmd.c
+++ src/cli_commands/move_cmd.c
@@ -28,7 +28,7 @@ int cmd_left(char *command_type, int cli_socket)
 int cmd_right(char *command_type, int cli_socket)
 {
     client_t *cli = get_client_by_socket(cli_socket);
-    
+
     (void)command_type;
     if (cli == NULL) {
         return -1;

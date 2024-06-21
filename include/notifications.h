e5ee1c4c1e2bcee73a96b108f8b3348e8c68db43
Noah Gibelli <noah.gibelli@epitech.eu>
2024-06-21 13:58:37 +0200

    [+] Notice the graphic when a client is ejected

--- include/notifications.h
+++ include/notifications.h
@@ -16,5 +16,6 @@ void notice_graphic_egg_destruction(int egg_id);
 void notice_graphic_client(client_t *cli, char *team_name);
 void notice_graphic_init(client_t *cli);
 void notice_graphic_move_cmd(client_t *cli);
+void notice_graphic_player_ejection(client_t *cli);
 
 #endif /* !NOTIFICATIONS_H_ */

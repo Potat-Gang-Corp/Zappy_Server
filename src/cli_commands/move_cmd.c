fb1dee403f5e0f0495847ccd0692afe73f71740e
Noah Gibelli <noah.gibelli@epitech.eu>
2024-06-21 11:36:31 +0200

    [+] sending to graphic when a client move

--- src/cli_commands/move_cmd.c
+++ src/cli_commands/move_cmd.c
@@ -11,6 +11,7 @@
 #include "../../include/server.h"
 #include "../../include/commands.h"
 #include "../../include/map.h"
+#include "../../include/notifications.h"
 
 int cmd_left(char *command_type, int cli_socket)
 {
@@ -76,5 +77,6 @@ int cmd_forward(char *command_type, int cli_socket)
     add_item_to_tiles(map->tiles[new_index], type);
     dprintf(cli->socket, "ok\n");
     cli->cd = 7 / game->freq;
+    notice_graphic_move_cmd(cli);
     return 0;
 }

7f85e2e899324a8de47a210b8fe04f54bc249418
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-06-24 13:58:25 +0200

    [+] fix timeout

--- src/server/login/handle_timeout.c
+++ src/server/login/handle_timeout.c
@@ -12,14 +12,13 @@
 void handle_timeout_login(void)
 {
     server_t *server = get_instance();
-    game_t *game = get_game_instance();
     client_t *cli = server->clients;
     client_t *next_cli = NULL;
 
     while (cli != NULL) {
         next_cli = cli->next;
         cli->cycle++;
-        if (cli->logged == false && cli->cycle == (5 * game->freq)) {
+        if (cli->logged == false && cli->cycle == 120) {
             dprintf(cli->socket, "Connexion Timeout\n");
             remove_client(cli->socket);
             cli = next_cli;
